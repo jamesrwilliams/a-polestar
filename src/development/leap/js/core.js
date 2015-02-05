d9336a9dbdf500297cf9aa837ec285c9cabc365b
James Williams <james@jamesrwilliams.co.uk>
2015-02-05 10:13:39 +0000

    Various updated to core project

--- src/development/leap/js/core.js
+++ src/development/leap/js/core.js
@@ -1,58 +1,41 @@
-/*
- * OTS AR Project
- *
- * Sandbox > Basic Stage Playground
- * @James_RWilliams
- */
+   /*
+	* OTS AR Project
+	*
+	* Sandbox > Basic Stage Playground
+	* @James_RWilliams
+	*/
  
-var cats = {};
-
-Leap.loop(function(frame) {
-
-  frame.hands.forEach(function(hand, index) {
+	
+	var output = document.getElementById('output');
+	Leap.loop(function (frame) {
     
-    var cat = ( cats[index] || (cats[index] = new Cat()) );    
-    cat.setTransform(hand.screenPosition());
+    
+    var controller = Leap.loop({enableGestures: true}, function(frame){
+  if(frame.valid && frame.gestures.length > 0){
+    frame.gestures.forEach(function(gesture){
+	    
+        switch (gesture.type){
+          case "circle":
+              ots_alert("Circle Gesture");
+              break;
+          case "keyTap":
+              ots_alert("Key Tap Gesture");
+              break;
+          case "screenTap":
+              ots_alert("Screen Tap Gesture");
+              break;
+          case "swipe":
+              ots_alert("Swipe Gesture");
+              break;
+        }
+    });
+  }
+});
+
     
   });
   
-}).use('screenPosition', {scale: 0.25});
-
-
-var Cat = function() {
-  var cat = this;
-  var img = document.createElement('img');
-  img.src = './img/icon.svg';
-  img.style.position = 'absolute';
-  img.onload = function () {
-    cat.setTransform([window.innerWidth/2,window.innerHeight/2], 0);
-    document.getElementById('stage').appendChild(img);
-  }
   
-  cat.setTransform = function(position, rotation) {
-
-    img.style.left = position[0] - img.width  / 2 + 'px';
-    img.style.top  = position[1] - img.height / 2 + 'px';
-    
-    img.style.height = 25 + 'px';
-    img.style.width = 25 + 'px';
-    	
-    img.style.webkitTransform = img.style.MozTransform = img.style.msTransform =
-    img.style.OTransform = img.style.transform;
-
-  };
-
-};
-
-ots_alert("Hello World");
-
-cats[0] = new Cat();
-
-// This allows us to move the cat even whilst in an iFrame.
-Leap.loopController.setBackground(true)
-
-
-
 
    /*
 	* Custom Alert Function
