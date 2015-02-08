d40cd62aa888f26f71f042c8abb627426baa0bda
James Williams <james@jamesrwilliams.co.uk>
2015-02-08 15:19:32 +0000

    Directory Restructure
    Progressed the development folder to be core src project. Updated
    README to contain repo file map. Created vendor directory for external
    dependancies. Fixed #2 - moved core-min.js up and renamed to form
    minified core.js file.

--- src/js/core.js
+++ src/js/core.js
@@ -15,7 +15,7 @@
 	
 	var al_red = 1;
 	var al_green = 1;
-	var al_blue = 0.8;
+	var al_blue = 1;
 	
 	var scene = new THREE.Scene(); 
 	var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 ); 
