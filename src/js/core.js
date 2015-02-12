56b8e89608574aae19ea561bc9f9b8f308735b4f
James Williams <james@jamesrwilliams.co.uk>
2015-02-12 11:42:27 +0000

    WIP

--- src/js/core.js
+++ src/js/core.js
@@ -21,10 +21,15 @@
 	var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 ); 
 	var renderer = new THREE.WebGLRenderer(); renderer.setSize( window.innerWidth, window.innerHeight );
 	
+	var gesture;
+	var marker_count = 0;
 	var cube;
 	
-	var controller = new Leap.Controller();
+	var controller = new Leap.Controller({enableGestures:true});
 	var controls = new THREE.LeapTrackballControls( camera , controller );
+	
+	var rotation_round_x;
+	var rotation_round_y;
 	
 	/*
 	 * onload function calls once the page has loaded fully.
@@ -38,6 +43,12 @@
 		animate();
 		
 	}
+	
+	/*
+	 * Initalise the Three.js objects and add LeapMotion controllers 
+	 *
+	 *
+	 */
 	
 	function init(){
 		
@@ -57,6 +68,7 @@
         controls.zoomDampening            = .6;
         controls.zoomCutoff               = .9;
         controls.zoomEnabled              = true;
+        controls.rotation._x			  = 0;
 
         controls.minZoom                  = 10;
         controls.maxZoom                  = 10;
@@ -77,60 +89,77 @@
 		
 		var render = function () { requestAnimationFrame( render ); 
 			
-
-			// Start animation loop..
-			leap_message("[VELOCITY] x = " + round2(controls.angularVelocity.x) + " y = " + round2(controls.angularVelocity.y) + " z = " + round2(controls.angularVelocity.z), 1);
-			leap_message("[ROTATION] x = " + round2(controls.rotation._x) 		+ " y = " + round2(controls.rotation._y) 	   + " z = " + round2(controls.rotation._z), 2);
-			leap_message("[ GESTURE] " + controller.lastValidFrame.hands[0], 3);
-	
-		}; 
+			rotation_round_x = round2(controls.rotation._x);
+			rotation_round_y = round2(controls.rotation._y);
 			
+			// Start animation loop..
+			
+			leap_message("[VELOCITY] x = " + round2(controls.angularVelocity.x) + " y = " + round2(controls.angularVelocity.y) + " z = " + round2(controls.angularVelocity.z),1);
+			leap_message("[ROTATION] x = " + round2(controls.rotation._x) 		+ " y = " + round2(controls.rotation._y) 	   + " z = " + round2(controls.rotation._z),2);
+			leap_message("[   DEPTH] = " + round2(controller.lastFrame.data),3);
+			leap_message("[ GESTURE] " + controller.lastValidFrame.hands[0],4);
+			
+			
+			// Slowly spin the object: not the camera to create orbit effect
+			cube.rotation.y += .0004;
+						
+		}; 
+	
 		render();
 		controller.connect();
+		
+		console.log(controller);
 			
 		window.addEventListener( 'resize', onWindowResize, false );
+		
+		window.setInterval(function(){
 			
-		console.log(controller.lastValidFrame);	
+			console.log("[TICK]");
+			
+		}, 5000);
+		
+		/*
+		 * Add Gesture Detection // Overrites the Spin Logic
+		 *
+		 *
+		 */
+		 
+	/*
+		 
+		var gesture_controller = Leap.loop({enableGestures: true}, function(frame){
+			
+			if(frame.valid && frame.gestures.length > 0){		
+				
+				//console.legnth(gesture);	
+				
+				frame.gestures.forEach(function(gesture){
+					
+					switch (gesture.type){
+					
+					case "keyTap":
+					console.log("Key Tap Gesture: " + gesture.position);;
+					create_tap_marker(gesture.position[0],gesture.position[1]);
+					
+					break;
+					
+					case "screenTap":
+					console.log("Screen Tap Gesture: " + gesture.position);
+					create_tap_marker(gesture.position[0],gesture.position[1]);
+					
+					
+					break;
+					
+					}
+				
+				});
+				
+			}
+			
+		});
+		
+		*/
 			
 	}
-
-	var gesture_controller = Leap.loop({enableGestures: true}, function(frame){
-		
-		console.legnth(gesture.duration);
-		
-		if(frame.valid && frame.gestures.length > 0){			
-			
-			frame.gestures.forEach(function(gesture){
-				
-				switch (gesture.type){
-				
-				case "circle":
-				console.log("Circle Gesture");
-				
-				break;
-				
-				case "keyTap":
-				console.log("Key Tap Gesture");
-				
-				break;
-				
-				case "screenTap":
-				console.log("Screen Tap Gesture");
-				
-				break;
-				
-				case "swipe":
-				console.log("Swipe Gesture");
-				break;
-				
-				}
-			
-			});
-			
-		}
-		
-	});
-	
 	
 	/*
 	 * Animate Function
@@ -162,6 +191,33 @@
 	
 	    renderer.setSize( window.innerWidth, window.innerHeight );
 	
+	}
+	
+	function create_tap_marker(x,y){
+		
+		/*
+		
+		Overview can be found here: https://developer.leapmotion.com/documentation/javascript/devguide/Leap_Overview.html
+		
+		X = Left/Right of sensor (Postiive = Right & Negitive = Left)
+		Y = Height realitive to sensor, 0 = flush to sensor 
+		Z = depth
+		
+		*/
+
+		var viewport_width = window.innerWidth;
+		
+		console.log(viewport_width);
+		
+		var output_length = x;
+		var output_height = (y/450*100);
+		
+		console.log("Length: " + output_length + " Height: " + output_height);
+		
+		$("#sensor").append("<span class='marker' style='bottom: " + output_height + "%;left: " + output_length + "px;'>" + marker_count + "</span>"); 
+		
+		marker_count++;
+		
 	}
 	
 	/*
